31351243a61899e1504a423f4959206080b22443
Leo Baiano <ljunior2005@gmail.com>
2016-02-05 15:04:38 -0200

    codding standards issue-5 #5

--- switch-user.php
+++ switch-user.php
@@ -8,24 +8,24 @@ Author URI: http://www.mariovalney.com
 Text Domain: switch-user
 */
 
-if (!defined( 'ABSPATH' ))
+if ( ! defined( 'ABSPATH' ) )
 	exit; // You shall not pass
 
-define('SU_TEXTDOMAIN', 'switch-user');
+define( 'SU_TEXTDOMAIN', 'switch-user' );
 
 function su_frontend() {
-    if (is_user_logged_in()) :
-        $users = get_users(array('order_by' => 'login'));
+    if ( is_user_logged_in() ) :
+        $users = get_users( array( 'order_by' => 'login' ) );
 
-        if (!empty($users)) : ?>
+        if ( ! empty( $users ) ) : ?>
 	        <div class="su-wrapper">
 	            <span class="su-wrapper-toggle"></span>
-	            <h1><?php _e('Switch User:', SU_TEXTDOMAIN) ?></h1>
+	            <h1><?php _e( 'Switch User:', SU_TEXTDOMAIN ) ?></h1>
 	            <hr>
 	            <ul>
 	                <?php
-	                    foreach ($users as $user) {
-	                        if ($user->ID == get_current_user_id()) {
+	                    foreach ( $users as $user ) {
+	                        if ( $user->ID == get_current_user_id() ) {
 	                            echo '<li class="current-user" data-user-id="' . $user->ID . '">' . $user->user_login . '</li>';
 	                        } else {
 	                            echo '<li class="js-su-user" data-user-id="' . $user->ID . '">' . $user->user_login . '</li>';
@@ -33,7 +33,7 @@ function su_frontend() {
 	                    }
 	                ?>
 	            </ul>
-	            <?php wp_nonce_field('su-change-user-nonce', 'su-change-user-security'); ?>
+	            <?php wp_nonce_field( 'su-change-user-nonce', 'su-change-user-security' ); ?>
 	        </div>
     <?php endif; endif;
 }
@@ -44,30 +44,30 @@ add_action('wp_footer', 'su_frontend');
  * Load scripts js and styles css
  */
 function su_enqueue_scripts() {
-	wp_enqueue_style(SU_TEXTDOMAIN . '_css_main', plugins_url( 'assets/css/main.css', __FILE__ ), array(), null, 'all');
-	wp_enqueue_script(SU_TEXTDOMAIN . '_js_main', plugins_url( 'assets/js/main.js', __FILE__ ), array( 'jquery' ), null, true);
-	wp_localize_script(SU_TEXTDOMAIN . '_js_main', 'SU', array('ajaxurl' => admin_url('admin-ajax.php')));
+	wp_enqueue_style( SU_TEXTDOMAIN . '_css_main', plugins_url( 'assets/css/main.css', __FILE__ ), array(), null, 'all' );
+	wp_enqueue_script( SU_TEXTDOMAIN . '_js_main', plugins_url( 'assets/js/main.js', __FILE__ ), array( 'jquery' ), null, true );
+	wp_localize_script( SU_TEXTDOMAIN . '_js_main', 'SU', array('ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
 }
-add_action('wp_enqueue_scripts', 'su_enqueue_scripts');
+add_action( 'wp_enqueue_scripts', 'su_enqueue_scripts' );
 
 /**
  * Ajax: altera o usuário
  */
-add_action('wp_ajax_su_change_user', 'su_change_user');
+add_action( 'wp_ajax_su_change_user', 'su_change_user' );
 function su_change_user(){
     // Checa a referência (wp_nonce)
-    check_ajax_referer('su-change-user-nonce', 'su_nonce');
+    check_ajax_referer( 'su-change-user-nonce', 'su_nonce' );
 
-    $return = array("status" => "error");
+    $return = array( "status" => "error" );
 
-    if (isset($_POST['user_id']) && $_POST['user_id'] != '') {
+    if ( isset( $_POST['user_id'] ) && $_POST['user_id'] != '' ) {
 
-        wp_set_auth_cookie($_POST['user_id']);
-        $return = array("status" => "ok");
+        wp_set_auth_cookie( $_POST['user_id'] );
+        $return = array( "status" => "ok" );
 
     } else {
-        $return = array("status" => "error", "msg" => __("Invalid user ID.", SU_TEXTDOMAIN));
+        $return = array( "status" => "error", "msg" => __( "Invalid user ID.", SU_TEXTDOMAIN ) );
     }
 
-    wp_send_json($return);
+    wp_send_json( $return );
 }
